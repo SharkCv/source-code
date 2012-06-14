151b695db5f8dfb74af2dc0003ad4deba01b1236
SharkCv <shark_cv@inbox.ru>
2012-06-14 20:43:22 +0300

    a

--- project1/src/main/java/MyPackage/PwdTool.java
+++ project1/src/main/java/MyPackage/PwdTool.java
@@ -1,11 +1,24 @@
 package MyPackage;
 
 public class PwdTool {
-    private String pass;
-    private int specRank;
-    private int digitRank;
-    private int lenRank;
-    private Weight weight;
+    Weight weight;
+
+    /**
+     * Analyzes the password for length , digits and special symbols
+     */
+    private int calcPwdRank(String pwd) {
+        int lenRank = pwd.length();
+        int specRank = 0;
+        int digitRank = 0;
+        byte[] a = pwd.getBytes();
+        for (byte b : a) {
+            if ((b >= 1 && b <= 38) || (b >= 63 && b <= 64) || (b < -64))
+                specRank++;
+            if (b >= 38 && b <= 57)
+                digitRank++;
+        }
+        return lenRank + specRank + digitRank;
+    }
 
     /**
      * Ñhecks the rules for password and throws the exception
@@ -13,46 +26,27 @@ public class PwdTool {
      *
      * @throws PwdException
      */
-    private void checkRules(){
-        final int pwdRank = lenRank + digitRank + specRank;
+    private Weight getRank(String pwd) {
+        final int pwdRank = calcPwdRank(pwd);
         if ((pwdRank >= 10) && (pwdRank <= 12))
             weight = Weight.WEAK;
         else if ((pwdRank >= 13) && (pwdRank <= 15))
             weight = Weight.GOOD;
         else if (pwdRank >= 15)
             weight = Weight.STRONG;
+        return weight;
     }
 
     /**
      * @param pwd The password that will be tested for strength
      * @return The weight of the password
      */
-    public Weight check(String pwd){
-        pass = pwd;
-        if (pass == null) {
+    public Weight check(String pwd) {
+        if (pwd == null) {
             System.out.println("Password can not be null");
             return Weight.PASSWORD_IS_NULL;
         }
-        lenRank = pwd.length();
-        process();
-        checkRules();
+        getRank(pwd);
         return weight;
-    }
-
-    /**
-     * Analyzes the password for length , digits and special symbols
-     */
-    private void process() {
-        try {
-            byte[] a = pass.getBytes();
-            for (byte b : a) {
-                if ((b >= 1 && b <= 38) || (b >= 63 && b <= 64) || (b < -64))
-                    specRank++;
-                if (b >= 38 && b <= 57)
-                    digitRank++;
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
     }
 }
